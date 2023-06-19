eab775a9755d0215cdf6078aa14e230d3218bb16
bigjuncn <446262388@qq.com>
2023-06-18 20:40:26 -0500

    2306182040

---
+++ resources/js/main.js
@@ -0,0 +1,62 @@
+var loginButton = document.getElementById('loginButton');
+var logoutButton = document.getElementById('logoutButton');
+// var sessionButton = document.getElementById('session');
+loginButton.addEventListener('click', function () {
+    const user = document.getElementById('usernameInput').value;
+    const pwd = document.getElementById('passwordInput').value;
+    // request login api by fetch api
+    fetch('http://localhost:3000/login', {
+        method: 'POST',
+        body: JSON.stringify({username: user, password: pwd}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json()).then(data => {
+        sessionStorage.setItem('token', data.token);
+        // valid the jwt work
+        fetch('http://localhost:3000/secure-endpoint', {
+            headers: {
+                'Authorization': sessionStorage.getItem('token')
+            }
+        }).then(response => {
+            if (response.ok) {
+                console.log('Access granted');
+            } else {
+                console.log('Access denied');
+            }
+        });
+        loginButton.style.display = 'none';
+        logoutButton.style.display = 'block';
+        var username = document.getElementById('usernameInput').value;
+        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('usernameDisplay').innerText = 'Logged in as: ' + username;
+        document.getElementById('userDisplay').style.display = 'block';
+    }).catch(error => {
+        alert('You should enter a valid or correct password');
+        console.log('Error:', error)
+    });
+});
+
+logoutButton.addEventListener('click', function () {
+    // logout action: remove token
+    sessionStorage.removeItem('token');
+    loginButton.style.display = 'block';
+    logoutButton.style.display = 'none';
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('userDisplay').style.display = 'none';
+});
+/*
+sessionButton.addEventListener('click', function () {
+    // test work
+    fetch('http://localhost:3000/secure-endpoint', {
+        headers: {
+            'Authorization': sessionStorage.getItem('token')
+        }
+    }).then(response => {
+        if (response.ok) {
+            console.log('Access granted');
+        } else {
+            console.log('Access denied');
+        }
+    });
+});*/
